ebf76f0e6fa66d32d5e47e2b5f1468513123f0af
Arab <abroimshoev@gmail.com>
2025-07-04 15:19:01 +0500

    Double check of brach and pull request

--- src/views/Debts.vue
+++ src/views/Debts.vue
@@ -326,7 +326,7 @@ const unpaidAmount = computed(() => totalPayments.value - paidAmount.value);
   font-weight: 500;
 }
 .download-btn {
-  background-color: #0f0c2e;
+  background-color: #6252fe;
   color: white;
   font-size: 14px;
   font-weight: 600;
